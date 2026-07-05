a8c6a1734702ed586eee9b5cbe185e756b17b801
sheetalmilind <sheetaldeshpande417@gmail.com>
2026-07-05 18:51:08 +0530

    Initial spring boot project

---
+++ synthetic-pr/synthetic-pr/src/main/java/com/codecatalyst/synthetic_pr/SyntheticPrApplication.java
@@ -0,0 +1,13 @@
+package com.codecatalyst.synthetic_pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntheticPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntheticPrApplication.class, args);
+	}
+
+}
---
+++ synthetic-pr/synthetic-pr/src/test/java/com/codecatalyst/synthetic_pr/SyntheticPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecatalyst.synthetic_pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntheticPrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
